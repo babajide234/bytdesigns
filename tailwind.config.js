be379808d8e10bfd15233bd6e68e8c63244321ae
babajide234 <babajide234@gmail.com>
2023-12-21 12:27:20 +0100

    deleted fonts folder

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./img/.{jpg,png,svg,gif}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:'#427478',
+        faded:'faded  green'
+      },
+      backgroundImage: {
+        'hero-img': 'url("../img/headerBg.png")',
+        'mission-img': 'url("../img/layer.png")',
+        'section-img': 'url("../img/section-img.png")',
+        'footer-img': 'url("../img/footer.png")',
+      },
+      fontFamily: {
+        'dm-sans': ['DM Sans', 'sans-serif'],
+        'dhurjati': ['Dhurjati', 'serif'],
+        'space-mono': ['Space Mono', 'monospace'],
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],
+      },
+      transitionProperty: {
+        'width': 'width'
+      },
+      keyframes: {
+        borderAnimation: {
+          from: { width: '0' },
+          to: { width: '100%' },
+        },
+      },
+      animation: {
+        'borderAnimation': 'borderAnimation 0.5s forwards ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+}
+
